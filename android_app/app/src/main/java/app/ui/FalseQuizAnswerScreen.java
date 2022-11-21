c6499531502760e61dd98c6b143b5c9aecc56502
red0phoenixx <104363035+red0phoenixx@users.noreply.github.com>
2022-11-21 19:25:22 +0100

    Add files via upload

---
+++ android_app/app/src/main/java/app/ui/FalseQuizAnswerScreen.java
@@ -0,0 +1,38 @@
+package app.ui;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+import app.MainActivity;
+import de.uwuwhatsthis.quizApp.ui.loginScreen.R;
+import app.ui.utils.Utils;
+
+public class FalseQuizAnswerScreen {
+    private MainActivity instance;
+
+    private TextView richtigeAntwort;
+    private Button back, next;
+
+    public FalseQuizAnswerScreen() {
+        instance = MainActivity.getInstance();
+        run();
+        }
+
+    private void run(){
+        instance.runOnUiThread(() -> instance.setContentView(R.layout.false_quiz_answer_screen));
+
+        instance.runOnUiThread(() -> {
+            richtigeAntwort = instance.findViewById(R.id.richtige_antwort);
+            back = instance.findViewById(R.id.back);
+            next = instance.findViewById(R.id.next);
+
+            richtigeAntwort.setText("Falsch!");
+        });
+        back.setOnClickListener(event -> {
+            new MapScreen();
+        });
+        next.setOnClickListener(event -> {
+            new QuizScreen();
+        });
+    };
+}
---
+++ android_app/app/src/main/java/app/ui/RightQuizAnswerScreen.java
@@ -0,0 +1,38 @@
+package app.ui;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+import app.MainActivity;
+import de.uwuwhatsthis.quizApp.ui.loginScreen.R;
+import app.ui.utils.Utils;
+
+public class RightQuizAnswerScreen {
+    private MainActivity instance;
+
+    private TextView richtigeAntwort;
+    private Button back, next;
+
+    public RightQuizAnswerScreen() {
+        instance = MainActivity.getInstance();
+        run();
+    }
+
+    private void run(){
+        instance.runOnUiThread(() -> instance.setContentView(R.layout.false_quiz_answer_screen));
+
+        instance.runOnUiThread(() -> {
+            richtig = instance.findViewById(R.id.richtig);
+            back = instance.findViewById(R.id.back);
+            next = instance.findViewById(R.id.next);
+
+            richtig.setText("Richtig!");
+        });
+        back.setOnClickListener(event -> {
+            new MapScreen();
+        });
+        next.setOnClickListener(event -> {
+            new QuizScreen();
+        });
+    };
+}
